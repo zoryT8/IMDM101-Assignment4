3dd3c212c2b5c04ad7f4085ff90d7e971a2b6b05
mangohead6 <jacobfmango@gmail.com>
2025-05-05 23:55:37 -0400

    Added Sounds & Music

---
+++ Assets/Scripts/RunningSound.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioControl : MonoBehaviour
+{
+    private AudioSource audioSource;
+    private Transform player;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        player = transform.parent;
+    }
+
+    void Update()
+    {
+        if (player != null && player.name == "Player (Mouse)")
+        {
+            bool isGrounded = Mathf.Abs(player.position.y - 0.1225509f) < 0.001f;
+
+            if (isGrounded)
+            {
+                if (!audioSource.isPlaying)
+                    audioSource.Play();
+            }
+            else
+            {
+                if (audioSource.isPlaying)
+                    audioSource.Stop();
+            }
+        }
+    }
+}
